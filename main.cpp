7e5179c5c539bbb7979cc6a8664f6d97366be37d
PlenkinAv <54805570+PlenkinAv@users.noreply.github.com>
2019-10-24 11:48:05 +0300

    Screen_Saver

--- main.cpp
+++ main.cpp
@@ -1,70 +1,166 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
+
+//#include <stdlib.h>
 using namespace std;
 
-void addWord (char** &words, int* &lengths, int &amountOfWords){
-    amountOfWords++;
-    char** temp = new char*[ amountOfWords ];
-    int* newLengths = new int[ amountOfWords ];
-    for (int i = 0; i < amountOfWords-1; i++) { temp[i] = words[i]; newLengths[i] = lengths[i]; };
-    temp [ amountOfWords-1 ] = new char [1];
-    temp [ amountOfWords-1 ] [0] = '\n';
-    newLengths[ amountOfWords-1 ] = 1;
-    delete words; delete lengths;
-    words = temp; lengths = newLengths;
-    return;}
+const int height = 15;
+const int width = 53;
 
-void pushChar (char ch, char **&words, int &amountOfWords, int* &lengths, int numberOfWord){
-    if ( numberOfWord > amountOfWords) {addWord (words, lengths, amountOfWords);}
-        lengths[numberOfWord-1]++;
-        char* temp = new char[ lengths[ numberOfWord-1 ] ];
-        for (int i=0; i<lengths[ numberOfWord-1 ]-1; i++) {temp[i] = words[numberOfWord-1][i];};
-        temp[lengths[ numberOfWord-1 ] -2] = ch;
-        delete words[numberOfWord-1];
-        words[numberOfWord-1] = temp;
-    return;}
+void currentFrame(char **screen);
 
- void readStringFromFile (FILE* fin, char** &words, int &amountOfWords, int* &lengths){
-    char ch = 0;
-    bool onWord = true;
-    if  ( ( (ch = fgetc(fin) ) == ' ') || (ch == EOF) ) {onWord = false;};
-    if ( ch != EOF) {
-        rewind(fin);
-        int i=1;
-        while  ( ( ch = fgetc(fin) ) != EOF )         
-            if ( ( ((ch >= '0') && (ch <= '9')) || ((ch >= 'A') && (ch <= 'Z')) ) || ((ch >= 'a') && (ch <= 'z')) ) {
-                pushChar(ch, words, amountOfWords, lengths, i);
-                onWord = true;}
-            else {
-                if ( (ch == ' ') && (onWord) )
-                    i++;
-                    onWord = false;};}
-    return;}
+void moveUp(char **&screen){
+    char* temp;
+    temp = screen[0];
 
-void sort (char** &words, int amountOfWords, int* lengths){
-    for(int i=0; i< amountOfWords-1; i++)
-        for(int j=i+1; j< amountOfWords; j++)
-            if ( lengths[i] > lengths[j] ) {
-                char* temp = words[i]; int tempLength = lengths[i];
-                words[i] = words[j]; lengths[i] = lengths[j];
-                words[j] = temp; lengths[j] = tempLength;
-            }
-    return;}
+    for (int i=0; i<height-1; i++)
+        screen[i] = screen[i+1];
 
-void outputWordsToFile(FILE *fout, char** words, int amountOfWords, int* lengths){
-    for (int i=0; i<amountOfWords; i++){
-        for (int j=0; j<lengths[i]-1; j++)
-            fprintf(fout, "%c", words[i][j]);
-        fprintf(fout, " ");};
-    return;}
+    screen[height-1] = temp;
+}
+
+void moveDown(char **&screen){
+    char* temp;
+
+    temp = screen[height-1];
+
+    for (int i=height-1; i>0; i--)
+        screen[i] = screen[i-1];
+
+    screen[0] = temp;
+}
+
+void moveRight(char **&screen){
+    char temp;
+    for (int i=0; i<height; i++){
+        temp = screen[i][width-1];
+        for (int j=width-1; j>0; j--){
+            screen[i][j] = screen[i][j-1];
+        }
+        screen[i][0] = temp;
+    }
+}
+
+void moveLeft(char **&screen){
+    char temp;
+    for (int i=0; i<height; i++){
+        temp = screen[i][0];
+        for (int j=0; j<width-1; j++){
+            screen[i][j] = screen[i][j+1];
+        }
+        screen[i][width-1] = temp;
+    }
+}
+
+void screenSaver(char *corners, int &x, int &y, char **&screen){
+    if (x>0){
+        if(corners[2]+1 <= width-1){ 
+            moveRight(screen); 
+            corners[2]++;
+            corners[0]++;
+        }
+        else{
+            x = -1;
+            moveLeft(screen);
+            corners[0]--;
+            corners[2]--;
+        };
+    }
+    else{
+        if(corners[0]-1 >= 0){ 
+            moveLeft(screen);
+            corners[0]--;
+            corners[2]--;
+        }
+        else{
+            x = 1;
+            moveRight(screen);
+            corners[0]++;
+            corners[2]++;
+        };
+    }
+
+    if (y>0){
+        if(corners[1]-1 >= 0){ 
+            moveUp(screen); 
+            corners[1]--;
+            corners[3]--;
+        }
+        else{
+            y = -1;
+            moveDown(screen);
+            corners[1]++;
+            corners[3]++;
+        };
+    }
+    else{
+        if(corners[3]+1 <= height-1){ 
+            moveDown(screen);
+            corners[1]++;
+            corners[3]++;
+        }
+        else{
+            y = 1;
+            moveUp(screen);
+            corners[1]--;
+            corners[3]--;
+        };
+    }
+}
+
+void currentFrame(char **screen){
+    string str;
+    for (int i=0; i<width+2; i++) str.push_back('=');
+    str.push_back('\n');
+    for (int i=0; i<height; i++){
+        str.push_back('|');
+        for (int j=0; j<width; j++)
+            str.push_back(screen[i][j]);
+        str.push_back('|');
+        str.push_back('\n');
+    }
+    for (int i=0; i<width+2; i++) str.push_back('=');
+    str.push_back('\n');
+    cout <<str;
+}
 
 int main(){
-    char** words; int* lengths; int amountOfWords = 0;
-    FILE *fin, *fout = fopen("output_file.txt", "w");
-    if ( !( fin = fopen ("input_file.txt", "r") ) ) {fprintf(fout, "Cannot open input file.");} else {
-        readStringFromFile(fin, words, amountOfWords, lengths);
-        sort(words, amountOfWords, lengths);
-        outputWordsToFile(fout, words, amountOfWords, lengths);
-    }
-    fclose(fin);
-    fclose(fout);
-    return 0;}
+    char **screen = new char*[height];
+    for (int i=0; i<height; i++){
+        screen[i] = new char[width];
+        for (int j=0; j<width; j++)
+            screen[i][j] = ' ';
+    };
+    int x = 1, y = -1;
+    char *corners = new char[4];
+    corners[0] = 0;
+    corners[1] = 0;
+    corners[2] = 6;
+    corners[3] = 1;
+
+    screen[0][0] = 'f';
+    screen[0][1] = 'u';
+    screen[0][2] = 'c';
+    screen[0][3] = 'k';
+    screen[0][4] = 'i';
+    screen[0][5] = 'n';
+    screen[0][6] = 'g';
+
+    screen[1][1] = 'c';
+    screen[1][2] = 'l';
+    screen[1][3] = 'o';
+    screen[1][4] = 'w';
+    screen[1][5] = 'n';
+
+
+system("pause");
+for (int i=0; i<100; i++)
+ {screenSaver(corners, x, y, screen);
+    currentFrame(screen);
+    for (unsigned j=0; j<0x02FFFFFF; j++)
+        {unsigned dum = 0xEFFFFFFF;
+        dum++;}
+};system("pause");
+return 0;  
+}
